31b12a8af8f4a8e543b25e3ce2d60b3e65234261
rekhaaluwala <rekha506.vita@gmail.com>
2021-03-30 17:20:08 +0530

    tags

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,9 @@
+import 'dart:html';
+
+import 'package:appone/degree.dart';
+import 'package:appone/inter.dart';
+import 'package:appone/tech.dart';
+import 'package:appone/tenth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -20,52 +26,129 @@ class _HomePageState extends State<HomePage> {
         primary: false,
         padding: const EdgeInsets.all(20),
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Card(
-                  child: Image.asset("images/asset/tent.jpg"),
-                ),
-              ),
-            ],
-          ),
-          Expanded(
-            flex: 4,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Card(
+                child: Column(
               children: <Widget>[
-                Container(
-                  child: Text(
-                    "THIS IS BIG TEST WHICH",
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 20,
-                    ),
-                  ),
+                Image.asset("Image/asset/tent.jpg",
+                    height: 100.0, width: 100.0),
+                Text("10th"),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => Ten(),
+                      ),
+                    );
+                  },
                 ),
               ],
+            )),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("Image/asset/ten2.jpg",
+                      height: 100.0, width: 100.0),
+                  Text("Intermediate"),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => Inter(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
           Container(
             padding: const EdgeInsets.all(8),
-            child: const Text('Intermediate'),
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("Image/asset/.jpg", height: 100.0, width: 100.0),
+                  Text("Degree"),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => Degree(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
           Container(
             padding: const EdgeInsets.all(8),
-            child: const Text('degree'),
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("Image/asset/tec.jpg",
+                      height: 100.0, width: 100.0),
+                  Text("B.Tech"),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => Tech(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
           Container(
             padding: const EdgeInsets.all(8),
-            child: const Text('B.Tech'),
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("Image/asset/dig.jpg",
+                      height: 100.0, width: 100.0),
+                  Text("PG"),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => Ten(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
           Container(
             padding: const EdgeInsets.all(8),
-            child: const Text('PG'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('M.Tech'),
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("Image/asset/tech.jpg",
+                      height: 100.0, width: 100.0),
+                  Text("M.Tech"),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => Ten(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
--- lib/tech.dart
+++ lib/tech.dart
@@ -1,16 +1,18 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class Degree extends StatefulWidget {
+class Tech extends StatefulWidget {
   @override
-  _DegreeState createState() => _DegreeState();
+  _TechState createState() => _TechState();
 }
 
-class _DegreeState extends State<Degree> {
+class _TechState extends State<Tech> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("List of courses offered"),
+        title: Text("career oppurtunities"),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30.0),
@@ -19,43 +21,43 @@ class _DegreeState extends State<Degree> {
             SizedBox(height: 20.0),
             ExpansionTile(
               title: Text(
-                "1. PMP Certification",
+                "1.M.tech/ME",
                 style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
               ),
               children: <Widget>[
                 ListTile(
                   title: Text(
-                      'PMP stands for Project Management Professional and PMP certification is one of the most distinguished professional certificates across the world for Project Management.'),
+                      'Both M.Tech/ M.E. (Masters of Technology/ Masters in Engineering) are postgraduate degree programmes offered by various engineering colleges in India.'),
                 ),
               ],
             ),
             ExpansionTile(
               title: Text(
-                "2. PGDM or M.Sc. in Business Analytics",
+                "2.MBA",
                 style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
               ),
               children: <Widget>[
                 ListTile(
                   title: Text(
-                      'Business Analytics, in essence, is a process that combines data analytics and business intelligence on a large set of data, to gather insights that aid in making future business strategies and plans.'),
+                      'Taking up an MBA is one of the most popular options for many B.Tech graduates. The course helps the technical graduates acquire management knowledge and leadership qualities, which is necessary for success in the corporate world.'),
                 ),
               ],
             ),
             ExpansionTile(
               title: Text(
-                "3. Master’s in Data Science",
+                "3.PGDM",
                 style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
               ),
               children: <Widget>[
                 ListTile(
                   title: Text(
-                      'Masters in Data Science is one of the best courses after graduation. Data science has deservedly earned its nickname as the sexiest job of the 21st century because, in a world where 2.5 quintillion bytes of data are produced every day, a professional who can organize this data to provide business solutions is indeed the hero!'),
+                      'For the students who do not wish to go for a full-time, two-year MBA, PGDM course can be the next best option. '),
                 ),
               ],
             ),
             ExpansionTile(
               title: Text(
-                "4. Chartered Financial Analyst",
+                "4. ",
                 style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
               ),
               children: <Widget>[
